e9672de130466df930e7eca480f98a9f31206c60
Garvit Saxena <garvit.saxena@scool.in>
2020-06-23 18:04:30 +0530

    vertical navbar done

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react'
 import VNavbar from "../components/VerticalNavbar";
-import Navbar from '../components/Navbar';
-
 class Home extends Component {
     render() {
         return (
             <div>
                 <VNavbar />
-                <Navbar />
-                <section>
-                    
-                </section>
-                Hello World
             </div>
         )
     }
