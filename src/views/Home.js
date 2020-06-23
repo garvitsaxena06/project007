aef48154a949f12256841b8983090da03510c0b0
Garvit Saxena <garvit.saxena@scool.in>
2020-06-23 18:00:14 +0530

    initial commit

---
+++ src/views/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import VNavbar from "../components/VerticalNavbar";
+import Navbar from '../components/Navbar';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <VNavbar />
+                <Navbar />
+                <section>
+                    
+                </section>
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default Home
